155ab3b72bf36ce315271348a0dd1e007c92f630
James Daniel Cadavona <jdpcadavona@gmail.com>
2026-03-18 17:10:17 -0700

    Added color indicators to metrics

--- frontend/src/components/metricCard.jsx
+++ frontend/src/components/metricCard.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './metricCard.css';
 
-const MetricCard = ({title, value, subtext}) => {
-    return (
-        <div className="metric__card">
-            <h2 className="metric__card__title">{title}</h2>
-            <p className="metric__card__value">{value}</p>
-            {subtext && <p className="metric__card__subtext">{subtext}</p>}
-        </div>
-    );
+const MetricCard = ({ title, value, subtext, status = 'healthy' }) => {
+  return (
+    <div className={`metric__card metric__card--${status}`}>
+      <h2 className="metric__card--title">{title}</h2>
+
+      <div
+        key={`${value}-${subtext ?? ''}`}
+        className="metric__card--content metric__card--content--animated"
+      >
+        <p className="metric__card--value">{value}</p>
+        {subtext && <p className="metric__card--subtext">{subtext}</p>}
+      </div>
+    </div>
+  );
 };
 
 export default MetricCard;
--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,28 +1,48 @@
 import React, { useState, useEffect} from 'react';
+import './dashboard.css'
 import { getMetrics } from '../services/api';
 import MetricCard from '../components/metricCard';
 
 const Dashboard = () => {
     const [metrics, setMetrics] = useState(null);
+    const [lastUpdated, setLastUpdated] = useState(null);
+
 
     useEffect(() => {
         const fetchMetricsData = async () => {
             try {
                 const data = await getMetrics();
                 setMetrics(data);
+                setLastUpdated(new Date());
             } catch(error) {
                 console.error(error);
             }
         };
         fetchMetricsData();
 
+        const interval = setInterval(() => {
+            fetchMetricsData();
+        }, 5000);
+
+        return () => clearInterval(interval);
     }, []);
+
+    const getMetricStatus = (value) => {
+        if (value >= 85) return 'critical';
+        if (value >= 60) return 'warning';
+        return 'healthy';
+    };
 
     return (
         <div className="dashboard">
-          <div className="dashboard-header">
+          <div className="dashboard__header">
             <h1>StackWatch</h1>
             <p>System Monitoring Dashboard</p>
+            <p className="last__updated">
+                {lastUpdated
+                ? `Last updated: ${lastUpdated.toLocaleString()}`
+                : 'Loading...'}
+            </p>
           </div>
       
           {!metrics ? (
@@ -32,12 +52,14 @@ const Dashboard = () => {
               <MetricCard
                 title="CPU Usage"
                 value={`${metrics.cpu.usage.toFixed(2)}%`}
+                status={getMetricStatus(metrics.cpu.usage)}
               />
       
               <MetricCard
                 title="Memory Usage"
                 value={`${metrics.memory.usage.toFixed(2)}%`}
                 subtext={`${metrics.memory.used.toFixed(2)} GB used • ${metrics.memory.available.toFixed(2)} GB available • ${metrics.memory.total.toFixed(2)} GB total`} 
+                status={getMetricStatus(metrics.memory.usage)}
                 />
       
               <MetricCard
@@ -51,6 +73,11 @@ const Dashboard = () => {
                   metrics.disk[0]
                     ? `${metrics.disk[0].used.toFixed(2)} GB / ${metrics.disk[0].total.toFixed(2)} GB`
                     : 'No disk data'
+                }
+                status={
+                    metrics.disk[0]
+                    ? getMetricStatus(metrics.disk[0].usage)
+                    : 'healthy'
                 }
               />
             </div>
