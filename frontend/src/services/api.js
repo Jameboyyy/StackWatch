f1b4e3ec65d7a75ab5526bffd28edee26b878566
James Daniel Cadavona <63523469+Jameboyyy@users.noreply.github.com>
2026-03-27 17:07:46 -0700

    Updated FrontEnd API calls

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://54.219.130.131:3000';
 
 export const getMetrics = async () => {
-    const response = await fetch('http://localhost:3000/metrics');
-    if(!response.ok) {
-        throw new Error('Failed to fetch metrics');
-    }
+  const response = await fetch(`${API_BASE_URL}/metrics`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch metrics');
+  }
 
-    return response.json();
+  return response.json();
 };
